5d4d2c08ac7c1bcbd30f0ab1017570d4be1a7fa5
Stu Thompson <s.thompson@cvent.com>
2019-07-30 15:28:04 -0700

    Build now creates a distribution folder.

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 INC="-I./inc/math -I./inc/render -I/usr/include/SDL2"
+INCDIR=./inc
 SRCROOT=./src
 SRCDIRS=(math render)
 BUILDDIR=./build
+DISTDIR=./dist
 OBJDIR=$BUILDDIR/obj
 LIBDIR=$BUILDDIR/lib
 OUTPUT=zenlib.a
@@ -10,12 +12,16 @@ CFLAGS="-D_REENTRANT --std=c++17"
 # Clean up previous builds
 echo "Cleaning build output"
 rm -rf build                                                # Remove build folder
+rm -rf dist                                                 # Remove distribution folder
 
-# Create build output directories
+# Create output directories
 echo "Creating build folders"
-mkdir ./build                                               # Create build folder
-mkdir ./build/obj                                           # Create obj folder
-mkdir ./build/lib                                           # Create lib folder
+mkdir $BUILDDIR                                             # Create build folder
+mkdir $OBJDIR                                               # Create obj folder
+mkdir $LIBDIR                                               # Create lib folder
+
+echo "Creating dist folders"
+mkdir $DISTDIR                                              # Create distribution folder
 
 # Compile object files
 echo "Compiling object files"
@@ -26,10 +32,10 @@ do
     for SRC in $SRCROOT/$SRCDIR/*.cpp                       # Iterate source files
     do
         OBJF=$(basename "$SRC")                             # Get just the file name (strip path)
-        OBJFNE="${OBJF%.*}"                                  # Get obj file name without .cpp ext
+        OBJFNE="${OBJF%.*}"                                 # Get obj file name without .cpp ext
         OBJ=$OBJFNE.o                                       # Create obj file name with .o extension
         echo "Compiling" $SRC "=>" $OBJDIR/$OBJ
-        g++ -c $SRC $INC -o $OBJDIR/$OBJ $CFLAGS  # Compile object file
+        g++ -c $SRC $INC -o $OBJDIR/$OBJ $CFLAGS            # Compile object file
         OBJFILES=$OBJFILES" "$OBJDIR/$OBJ                   # Concatenate to list of obj files
     done
 done
@@ -37,3 +43,10 @@ done
 # Compile library
 echo "Linking" $LIBDIR/$OUTPUT "from" $OBJFILES
 ar rcs $LIBDIR/$OUTPUT $OBJFILES                            # Compile static library
+
+# Copy output to dist folder
+echo "Copying files to distribution folder"
+cp -r $INCDIR $DISTDIR
+cp -r $LIBDIR $DISTDIR
+
+# Copy lib into dist folder

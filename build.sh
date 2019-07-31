ac6b710b396ff73c8d9f02281e76aae60a246584
Stu Thompson <s.thompson@cvent.com>
2019-07-30 22:13:59 -0700

    Build is now platform/task specific.

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
-INC="-I./inc -I./inc/math -I./inc/render -I/usr/include/SDL2"
+PLATFORM=$1
+TASK=$2
+
+# Validate arguments
+if [ "$PLATFORM" != "linux" ] && [ "$PLATFORM" != "osx" ]; then
+    echo "Usage: sh ./build.sh [platform]"
+    exit 0
+fi
+
+# Base-config
+INC="-I./inc -I./inc/math -I./inc/render"
 INCDIR=./inc
 SRCROOT=./src
 SRCDIRS=(math render)
@@ -7,43 +17,61 @@ DISTDIR=./dist
 OBJDIR=$BUILDDIR/obj
 LIBDIR=$BUILDDIR/lib
 OUTPUT=libzenixel.a
-CFLAGS="-D_REENTRANT --std=c++17"
+CFLAGS="-std=c++17"
 
-# Clean up previous builds
-echo "Cleaning build output"
-rm -rf build                                                # Remove build folder
-rm -rf dist                                                 # Remove distribution folder
+# Set platform-specific variables
+if [ "$PLATFORM" == "linux" ]; then
+    echo "Targeting Linux"
+    PLATFORM="linux"
+    INC="$INC -I/usr/include/SDL2"
+    LIB="-L/usr/lib -lSDL2 -lSDL2_ttf"
+fi
+if [ "$PLATFORM" == "osx" ]; then
+    echo "Targeting OSX"
+    PLATFORM="osx"
+    INC="$INC -I/usr/local/include/SDL2"
+    LIB="-L/usr/local/lib -lSDL2 -lSDL2_ttf"
+fi
 
-# Create output directories
-echo "Creating build folders"
-mkdir $BUILDDIR                                             # Create build folder
-mkdir $OBJDIR                                               # Create obj folder
-mkdir $LIBDIR                                               # Create lib folder
-echo "Creating dist folder"
-mkdir $DISTDIR                                              # Create distribution folder
+if [ "$TASK" == "clean" ] || [ "$TASK" == "" ]; then
+    # Clean up previous build
+    echo "Cleaning build output"
+    rm -rf build                                                # Remove build folder
+    rm -rf dist                                                 # Remove distribution folder
+fi
 
-# Compile object files
-echo "Compiling object files"
-OBJFILES=""
-for SRCDIR in ${SRCDIRS[@]}
-do
-    echo "Compiling src in" $SRCROOT/$SRCDIR
-    for SRC in $SRCROOT/$SRCDIR/*.cpp                       # Iterate source files
+if [ "$TASK" == "build" ] || [ "$TASK" == "" ]; then
+    # Create output directories
+    echo "Creating build folders"
+    mkdir $BUILDDIR                                             # Create build folder
+    mkdir $OBJDIR                                               # Create obj folder
+    mkdir $LIBDIR                                               # Create lib folder
+    echo "Creating dist folder"
+    mkdir $DISTDIR                                              # Create distribution folder
+
+    # Compile object files
+    echo "Compiling object files"
+    OBJFILES=""
+    for SRCDIR in ${SRCDIRS[@]}
     do
-        OBJF=$(basename "$SRC")                             # Get just the file name (strip path)
-        OBJFNE="${OBJF%.*}"                                 # Get obj file name without .cpp ext
-        OBJ=$OBJFNE.o                                       # Create obj file name with .o extension
-        echo "Compiling" $SRC "=>" $OBJDIR/$OBJ
-        g++ -c $SRC $INC -o $OBJDIR/$OBJ $CFLAGS            # Compile object file
-        OBJFILES=$OBJFILES" "$OBJDIR/$OBJ                   # Concatenate to list of obj files
+        echo "Compiling src in" $SRCROOT/$SRCDIR
+        for SRC in $SRCROOT/$SRCDIR/*.cpp                       # Iterate source files
+        do
+            OBJF=$(basename "$SRC")                             # Get just the file name (strip path)
+            OBJFNE="${OBJF%.*}"                                 # Get obj file name without .cpp ext
+            OBJ=$OBJFNE.o                                       # Create obj file name with .o extension
+            echo "Compiling" $SRC "=>" $OBJDIR/$OBJ
+            g++ -c $SRC $INC -o $OBJDIR/$OBJ $CFLAGS            # Compile object file
+            OBJFILES=$OBJFILES" "$OBJDIR/$OBJ                   # Concatenate to list of obj files
+        done
     done
-done
 
-# Compile library
-echo "Linking" $LIBDIR/$OUTPUT "from" $OBJFILES
-ar rcs $LIBDIR/$OUTPUT $OBJFILES                            # Compile static library
+    # Compile library
+    echo "Linking" $LIBDIR/$OUTPUT "from" $OBJFILES
+    ar rcs $LIBDIR/$OUTPUT $OBJFILES                            # Compile static library
 
-# Copy output to dist folder
-echo "Copying files to distribution folder"
-cp -r $INCDIR $DISTDIR
-cp -r $LIBDIR $DISTDIR
+    # Copy output to dist folder
+    echo "Copying files to distribution folder"
+    cp -r $INCDIR $DISTDIR
+    cp -r $LIBDIR $DISTDIR
+fi
